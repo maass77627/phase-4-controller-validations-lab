a2ac887ec3cfc0df9a9cab9722c42d9636929859
kelly_maass <kmaass77627@gmail.com>
2024-09-09 21:01:13 -0500

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  # validates :title, presence: true
+  # validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  # validates :content, length: {minimum: 100}
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
@@ -9,12 +13,17 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
 
   private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+
+  end
 
   def post_params
     params.permit(:category, :content, :title)
